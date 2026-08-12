6935e4e79822269af7a8f11367e3d754a3b259cb
NguyenHaiDang06 <dn78210@gmail.com>
2026-08-12 16:42:02 +0700

    Finish deep.py

---
+++ Week0/deep.py
@@ -0,0 +1,7 @@
+x=input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
+
+match x.strip().lower():
+    case "42" | "forty-two" | "forty two":
+        print("Yes")
+    case _:
+        print("No")    
