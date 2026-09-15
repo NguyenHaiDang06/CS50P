b7ff0ca523d5f77df8796828877c2421438ffc2f
NguyenHaiDang06 <dn78210@gmail.com>
2026-09-15 18:56:52 +0700

    Finish test_bank1.py

---
+++ Week0/test_bank1.py
@@ -0,0 +1,9 @@
+from bank import value
+
+def test_hello():
+    assert value("hello") == 0
+    assert value("HELLO") == 0
+def test_h():
+    assert value("hi") == 20
+def test_other():
+    assert value("good morning") == 100
